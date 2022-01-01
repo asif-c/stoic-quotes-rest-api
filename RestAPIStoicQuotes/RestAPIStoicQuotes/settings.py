fe8524804be10f50fe86a350c223de0656d90737
Asif Choudhury <asifikchoudhury@gmail.com>
2022-01-01 07:48:49 -0800

    Update settings.py

--- RestAPIStoicQuotes/RestAPIStoicQuotes/settings.py
+++ RestAPIStoicQuotes/RestAPIStoicQuotes/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u#3*f^%=w@d2!ijrhj441pzj^&*ae30*rdpln5cywy37b6n^qw'
+SECRET_KEY = ':):):):):):)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'StoicQuotes',
-        'USER':'postgres',
-        'PASSWORD':'adi',
+        'USER':'boozbooz',
+        'PASSWORD':'boozbooz1',
         'HOST':'127.0.0.1',
         'PORT':'',
     }
